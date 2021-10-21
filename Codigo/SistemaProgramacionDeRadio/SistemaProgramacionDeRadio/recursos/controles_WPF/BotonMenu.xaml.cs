b2bc008f6eb5e543a651b0613d76a2e6f698afdd
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2021-10-20 20:49:00 -0500

    Control BotonMenu creado.
    Control creado para el menu de la pantalla principal.

---
+++ Codigo/SistemaProgramacionDeRadio/SistemaProgramacionDeRadio/recursos/controles_WPF/BotonMenu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SistemaProgramacionDeRadio.recursos.controles_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para BotonMenu.xaml
+    /// </summary>
+    public partial class BotonMenu : UserControl
+    {
+        public BotonMenu()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public String Text { get; set; }
+        public String SourceImage { get; set; }
+
+
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(MouseUpEvent, value); }
+            remove { RemoveHandler(MouseUpEvent, value); }
+        }
+    }
+}
